86baa0da909156009e11fd8f7157bb3500d9eebf
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2021-08-02 13:17:37 +0200

    hide unstable code

--- app/src/main/java/by/treekonn/moq/MovieRepository.kt
+++ app/src/main/java/by/treekonn/moq/MovieRepository.kt
@@ -1,20 +1,20 @@
-package by.treekonn.moq
-
-import by.treekonn.moq.api.TmdbApi
-import by.treekonn.moq.api.TmdbMovie
-
-class MovieRepository(private val api: TmdbApi) : BaseRepository() {
-
-    suspend fun getPopularMovies(): MutableList<TmdbMovie>? {
-
-        //safeApiCall is defined in BaseRepository.kt (https://gist.github.com/navi25/67176730f5595b3f1fb5095062a92f15)
-        val movieResponse = safeApiCall(
-            call = { api.getPopularMovies().await() },
-            errorMessage = "Error Fetching Popular Movies"
-        )
-
-        return movieResponse?.results.toMutableList();
-
-    }
-
-}
+//package by.treekonn.moq
+//
+//import by.treekonn.moq.api.TmdbApi
+//import by.treekonn.moq.api.TmdbMovie
+//
+//class MovieRepository(private val api: TmdbApi) : BaseRepository() {
+//
+//    suspend fun getPopularMovies(): MutableList<TmdbMovie>? {
+//
+//        //safeApiCall is defined in BaseRepository.kt (https://gist.github.com/navi25/67176730f5595b3f1fb5095062a92f15)
+//        val movieResponse = safeApiCall(
+//            call = { api.getPopularMovies().await() },
+//            errorMessage = "Error Fetching Popular Movies"
+//        )
+//
+//        return movieResponse?.results.toMutableList();
+//
+//    }
+//
+//}
--- app/src/main/java/by/treekonn/moq/TmdbViewModel.kt
+++ app/src/main/java/by/treekonn/moq/TmdbViewModel.kt
@@ -1,34 +1,34 @@
-package by.treekonn.moq
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import by.treekonn.moq.api.ApiFactory
-import by.treekonn.moq.api.TmdbMovie
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
-
-class TmdbViewModel : ViewModel(){
-  
-    private val parentJob = Job()
-
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.Default
-
-    private val scope = CoroutineScope(coroutineContext)
-
-    private val repository : MovieRepository = MovieRepository(ApiFactory.tmdbApi)
-    
-
-    val popularMoviesLiveData = MutableLiveData<MutableList<TmdbMovie>>()
-
-    fun fetchMovies(){
-        scope.launch {
-            val popularMovies = repository.getPopularMovies()
-            popularMoviesLiveData.postValue(popularMovies)
-        }
-    }
-
-
-    fun cancelAllRequests() = coroutineContext.cancel()
-
-}
+//package by.treekonn.moq
+//
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//import by.treekonn.moq.api.ApiFactory
+//import by.treekonn.moq.api.TmdbMovie
+//import kotlinx.coroutines.*
+//import kotlin.coroutines.CoroutineContext
+//
+//class TmdbViewModel : ViewModel(){
+//
+//    private val parentJob = Job()
+//
+//    private val coroutineContext: CoroutineContext
+//        get() = parentJob + Dispatchers.Default
+//
+//    private val scope = CoroutineScope(coroutineContext)
+//
+//    private val repository : MovieRepository = MovieRepository(ApiFactory.tmdbApi)
+//
+//
+//    val popularMoviesLiveData = MutableLiveData<MutableList<TmdbMovie>>()
+//
+//    fun fetchMovies(){
+//        scope.launch {
+//            val popularMovies = repository.getPopularMovies()
+//            popularMoviesLiveData.postValue(popularMovies)
+//        }
+//    }
+//
+//
+//    fun cancelAllRequests() = coroutineContext.cancel()
+//
+//}
